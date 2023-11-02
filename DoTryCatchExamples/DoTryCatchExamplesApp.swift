c44b12ea0337599462efaf683143a9cf4b400cbf
Vladimir Kravets <natccorp@gmail.com>
2023-11-02 15:06:26 +0200

    Initial Commit

---
+++ DoTryCatchExamples/DoTryCatchExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchExamplesApp.swift
+//  DoTryCatchExamples
+//
+//  Created by Vladimir Kravets on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
